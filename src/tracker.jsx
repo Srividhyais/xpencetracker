65b617fbbfa9b5ff944a50b1313cfcc9673e8bde
Srividhya Radhakrishnan <vidhya@Srividhyas-MacBook-Air.local>
2025-06-11 09:26:36 -0400

    few changes

--- src/tracker.jsx
+++ src/tracker.jsx
@@ -210,7 +210,7 @@ export default function Trackers() {
                     >
                         <h3 style={{ marginBottom: '20px' }}>Add Balance</h3>
                         <div style={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
-                            <input className="balanceInput" type='text' value={addBalance} onChange={(e) => setAddBalance(e.target.value)} placeholder='Income Amount' />
+                            <input className="balanceInput" type='number' value={addBalance} onChange={(e) => setAddBalance(e.target.value)} placeholder='Income Amount' />
                             <button
                                 onClick={handleAddBalance}
                                 style={{
@@ -290,7 +290,7 @@ export default function Trackers() {
                         <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
                             <div className='expenseInnerDiv'>
                                 <input
-
+                                    name='title'
                                     type='text'
                                     value={expense.title}
                                     onChange={(e) => setExpense({ ...expense, title: e.target.value })}
